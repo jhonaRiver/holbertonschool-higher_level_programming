8e35292f82cbb9c5cb078631ee1052be55cafb1f
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-01-05 14:28:01 +0000

    Modified file task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,36 @@
 #include "lists.h"
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * _compare_extreme_nodes - function that use a recursion to check palindromic
+ * @head: head of linked list
+ * @tail: the node that will be the tail of a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int _compare_extreme_nodes(listint_t **head, listint_t *tail)
+{
+	int result;
+
+	if (!tail)
+		return (1);
+
+	result = _compare_extreme_nodes(head, tail->next);
+	if (result == 0)
+		return (0);
+	result = ((*head)->n == tail->n);
+	*head = (*head)->next;
+
+	return (result);
+}
+
+/**
+ * is_palindrome - is palindrome a simply linked list without using a while
+ *
+ * @head: head of linked list
+ * Return: call another function to call the last node in less time
+ * 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
-
-	if (head == NULL || *head == NULL)
-		return (1);
-
-	temp = *head;
-	while (temp) /* get len of list */
-	{
-		temp = temp->next;
-		len++;
-	}
-	return (0);
+	if (!head)
+		return (0);
+	return (_compare_extreme_nodes(head, *head));
 }
