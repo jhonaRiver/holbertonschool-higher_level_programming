ec12137541130a8506b895fff45a17f31f61e6db
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-01-18 14:09:11 +0000

    Added file task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Square module - provides a class Square with initialized size
+"""
+
+
+class Square:
+    """
+    Class Square with initialized size
+    """
+    def __init__(self, size):
+        self.__size = size
