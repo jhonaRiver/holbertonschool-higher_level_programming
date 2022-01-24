dbdb956e94ee933dab9da5293b4503211bd991a2
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-01-24 14:53:07 +0000

    Added file task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Rectangle module - provides a class Rectangle with initialized values
+Raises errors for invalid input
+Getters and Setters for width and height
+"""
+
+
+class Rectangle:
+    """
+    Defines a Rectangle with initialized values or 0 default
+    Raises errors for invalid input
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
