3011aef687422f81fd99c56f385d6aaf9893394a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-01-31 12:41:18 -0400

    Added file task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+area module
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Raises exception
+        """
+        raise Exception("area() is not implemented")
