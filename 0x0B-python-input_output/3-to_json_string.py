87a913b82b0f7fd15bb11b82355fba717f9e6426
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-02-01 12:05:07 -0400

    Added file task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module to_json_string
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
