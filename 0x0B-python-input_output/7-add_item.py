da466726066e2adcb4eeea6f987792814bd92f2e
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-02-02 13:49:29 -0400

    Modified file task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,9 @@ Module adds all arguments to a Python list, and then save them to a file
 
 
 from sys import argv
+"""
+import argv
+"""
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
