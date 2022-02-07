c2e7289c2c72e5b976e9a477eaa754de38e951ef
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-02-07 10:53:09 -0400

    Added file task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -119,3 +119,20 @@ class Rectangle(Base):
         """
         return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(self.id,
                 self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns arguments to each attribute
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'width', 'height', 'x', 'y']
+        if argc > 5:
+            argc = 5
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
