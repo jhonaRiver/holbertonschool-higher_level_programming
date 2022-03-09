07673927be225e5c56a6bcd523a67b38eb16a505
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-03-09 20:24:26 +0000

    Modified file task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT tv_shows.title
+SELECT tv_shows.title AS title
 FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+INNER JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
