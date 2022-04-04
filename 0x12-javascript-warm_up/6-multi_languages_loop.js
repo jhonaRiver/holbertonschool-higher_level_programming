0850e638fe61ad6b087c7fcffd4f117a5c82e321
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-04 21:17:42 +0000

    Added file task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that prints 3 lines
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
+for (idx = 0; idx < 3; idx++) {
+  console.log(arr[idx]);
+}
