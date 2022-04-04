07b7f3e81118fed48afc27edb4cb30d58bd125c5
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-04 21:44:36 +0000

    Added file task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that computes and prints a factorial
+function factorial (n) {
+  if (n <= 1) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+let num = parseInt(process.argv[2]);
+if (isNaN(num)) {
+  num = 1;
+}
+console.log(factorial(num));
