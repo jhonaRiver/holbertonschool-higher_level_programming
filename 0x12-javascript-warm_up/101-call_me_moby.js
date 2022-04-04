a8729df3d2b55555e9944e609ffcc3902efcba2a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-04 23:15:20 +0000

    Added file task 15

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that executes x times a function
+function callMeMoby (x, theFunction) {
+  let num = 0;
+  while (num < x) {
+    theFunction();
+    num++;
+  }
+}
+exports.callMeMoby = callMeMoby;
