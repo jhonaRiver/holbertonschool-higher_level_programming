cf96f86a8fab5564abaedbbcb85c49e836c8f4f8
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-04 21:09:37 +0000

    Added file task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints My number: <first argument converted in integer>
+const argNum = process.argv[2];
+if (isNaN(argNum)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + argNum);
+}
