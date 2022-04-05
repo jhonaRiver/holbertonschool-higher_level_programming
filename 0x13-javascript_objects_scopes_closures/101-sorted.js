c4f002cf6a0c922710516772223f0ce4ee407fa2
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-05 17:30:11 +0000

    Added file task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const key in dict) {
+  if (newdict[dict[key]] === undefined) {
+    newdict[dict[key]] = [];
+  }
+  newdict[dict[key]].push(key);
+}
+console.log(newdict);
