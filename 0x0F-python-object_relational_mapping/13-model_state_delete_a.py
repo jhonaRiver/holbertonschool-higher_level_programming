40c98ba3bdeea1513681ba16ac20a20dab18f508
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-09 15:33:42 +0000

    Modified file task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-'''
-script that deletes all State objects with a name containing the letter a from
-the database hbtn_0e_6_usa
-'''
+''' Script that deletes all state obj with a name with the
+letter a from the database '''
 
-
+import MySQLdb
 from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine, orm
-import sqlalchemy
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(*argv[1:4]), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = orm.sessionmaker(bind=engine)()
-
-    states = session.query(State).all()
-
-    for state in states:
-        if ('a' in state.name):
-            session.delete(state)
-
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State).from_statement(text(
+        "SELECT * "
+        "FROM states "
+        "WHERE name REGEXP '^.*a.*' "
+    )).all()
+    for state in rows:
+        session.delete(state)
     session.commit()
-
     session.close()
