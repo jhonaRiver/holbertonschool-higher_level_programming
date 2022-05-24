4eedbcfbdaa79713b9e24ee420990cdeadfd1c75
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-05-24 15:38:37 +0000

    Added file task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file
+const request = require('request');
+const f = require('fs');
+const webpage = process.argv[2];
+const file = process.argv[3];
+request(webpage, function (error, res, content) {
+  if (error) {
+    console.error('error:', error);
+  }
+  f.writeFile(file, content, 'utf-8', (errorB) => {
+    if (errorB) {
+      console.error('error:', errorB);
+    }
+  });
+});
